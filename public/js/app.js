95b1e0fc9526d1a8d86c3278c7330ce87ff10b8c
Nick Escallon <nickescallon@gmail.com>
2014-03-03 20:30:30 -0800

    fixed whitespace on app.js

--- public/js/app.js
+++ public/js/app.js
@@ -2,17 +2,19 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+angular.module('myApp', ['ngRoute','myApp.filters','myApp.services','myApp.directives','myApp.controllers'])
+  .config(function($routeProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
   $routeProvider
-    .when('/view1', {templateUrl: 'templates/partial1.html', controller: 'MyCtrl1'})
-    .when('/view2', {templateUrl: 'templates/partial2.html', controller: 'MyCtrl2'})
-    .otherwise({redirectTo: '/view1'});
-}]);
+    .when('/view1', {
+      templateUrl: 'templates/partial1.html',
+       controller: 'MyCtrl1'
+     })
+    .when('/view2', {
+      templateUrl: 'templates/partial2.html',
+       controller: 'MyCtrl2'
+     })
+    .otherwise({
+      redirectTo: '/view1'
+    });
+});
