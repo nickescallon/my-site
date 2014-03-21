2336dc59bac2aff0e0743a9add171670974e8eb3
Nick Escallon <nickescallon@gmail.com>
2014-03-20 23:44:04 -0700

    created the home controller, added drawing animation to home partial

---
+++ public/js/FrameController.js
@@ -0,0 +1,8 @@
+angular.module('myApp')
+  .controller('FrameController', function ($scope) {
+    $scope.menu = [
+      "Projects",
+      "Resume",
+      "About"
+    ];
+  });
--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,16 @@
-// 'use strict';
-
-
-// Declare app level module which depends on filters, and services
-angular.module('myApp', ['ngRoute','myApp.filters','myApp.services','myApp.directives','myApp.controllers'])
+angular.module('myApp', ['ngRoute'])
   .config(function($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
-  $routeProvider
-    .when('/view1', {
-      templateUrl: 'templates/partial1.html',
-       controller: 'MyCtrl1'
-     })
-    .when('/view2', {
-      templateUrl: 'templates/partial2.html',
-       controller: 'MyCtrl2'
-     })
-    .otherwise({
-      redirectTo: '/view1'
-    });
+    $locationProvider.html5Mode(true);
+    $routeProvider
+      .when('/', {
+        templateUrl: 'templates/partial1.html',
+         controller: 'HomeController'
+       })
+      // .when('/view2', {
+      //   templateUrl: 'templates/partial2.html',
+      //    controller: 'MyCtrl2'
+      //  })
+      .otherwise({
+        redirectTo: '/'
+      });
 });
