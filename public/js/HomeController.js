f2eb882505617ad9a8794e567fb75400ad7c9933
Nick Escallon <nickescallon@gmail.com>
2014-03-21 02:26:10 -0700

    added d3 and d3-tip, also integrated hrConnect

--- public/js/HomeController.js
+++ public/js/HomeController.js
@@ -1,4 +1,8 @@
 angular.module('myApp').
-  controller('HomeController', function ($scope) {
+  controller('HomeController', function ($scope, $timeout) {
     $scope.one = "HELLO 1";
+    $scope.ready = false;
+    $timeout(function(){
+      $scope.ready=true;
+    }, 3000);
   });
