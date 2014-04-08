d9ecf1a86baa4b380db9362038837cd4a610e54c
Nick Escallon <nickescallon@gmail.com>
2014-04-08 10:12:24 -0400

    fixed indentation in projects controller, created a file for the image service

--- public/js/controllers/ProjectsController.js
+++ public/js/controllers/ProjectsController.js
@@ -1,6 +1,6 @@
 angular.module('myApp')
-.controller('ProjectsController', function ($scope, $route, $location, $anchorScroll) {
-	$location.hash('bottom-page');
-    $anchorScroll();
-    $location.hash('');
-});
+  .controller('ProjectsController', function ($scope, $route, $location, $anchorScroll) {
+  	$location.hash('bottom-page');
+      $anchorScroll();
+      $location.hash('');
+  });
