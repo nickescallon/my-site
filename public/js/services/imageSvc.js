e463fc44f458bfb9ced689960ead46ef3c03ad56
Nick Escallon <nickescallon@gmail.com>
2014-04-08 10:40:12 -0400

    added image links and titles to imgSvc

---
+++ public/js/services/imageSvc.js
@@ -0,0 +1,12 @@
+angular.module('myApp')
+  .service('imgSvc', function() {
+    this.images = [
+      {title: 'ammo.io', url: 'images/square/ammo_square.png'},
+      {title: 'Acompli Dashboard', url: 'images/square/acompli_square.png'},
+      {title: 'Hack Reactor Connect', url: 'images/square/hrConnect_square.png'},
+      {title: 'veronicacastellucci.com', url: 'images/square/vc_square.png'},
+      {title: 'Uptime', url: 'images/square/uptime_square.png'},
+      {title: 'Rotating D3 Globe', url: 'images/square/earth_square.png'},
+      {title: 'D3 data structures', url: 'images/square/stack_queue_square.png'}
+    ];
+  });
