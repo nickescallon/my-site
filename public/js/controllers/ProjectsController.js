c7d70260ad1d9a8a10cb5b7313864e570da9cce5
Nick Escallon <nickescallon@gmail.com>
2014-04-08 10:09:23 -0400

    added anchor scrolling to projects controller

--- public/js/controllers/ProjectsController.js
+++ public/js/controllers/ProjectsController.js
@@ -1,4 +1,6 @@
 angular.module('myApp')
 .controller('ProjectsController', function ($scope, $route, $location, $anchorScroll) {
-
+	$location.hash('bottom-page');
+    $anchorScroll();
+    $location.hash('');
 });
